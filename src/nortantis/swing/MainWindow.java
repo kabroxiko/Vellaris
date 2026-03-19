18f021006460120aef03f4e092d22a8ce7dc2cfc
Joseph <jeheydorn@gmail.com>
2026-03-19 14:19:21 -0600

    Apple adjustments

--- src/nortantis/swing/MainWindow.java
+++ src/nortantis/swing/MainWindow.java
@@ -2231,6 +2231,8 @@ public class MainWindow extends JFrame implements ILoggerTarget
 	 */
 	public static void main(String[] args)
 	{
+		System.setProperty("apple.awt.application.name", "Nortantis");
+
 		PlatformFactory.setInstance(new AwtFactory());
 
 		Translation.initialize();
